0b270334f380757e65326434491056e334463980
gabriel-suela <gscsuela@gmail.com>
2024-04-25 19:19:45 -0300

    remove unused colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,55 +22,15 @@ return {
         disable_background = true,
         styles = {
           bold = true,
-          italic = true,
-          transparency = false,
+          italic = false,
+          transparency = true,
         },
       }
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = function()
-      return {
-        transparent_background = true,
-      }
-    end,
-  },
-  {
-    "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("everforest").setup({
-        background = "hard",
-        colours_override = function(palette)
-          palette.bg0 = "#1e2326"
-        end,
-      })
     end,
   },
   {
     "LunarVim/darkplus.nvim",
     lazy = false,
     priority = 1000,
-  },
-  {
-    "sainnhe/gruvbox-material",
-    name = "gruvbox-material",
-    event = "VeryLazy",
-    config = function()
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_disable_italic_comment = 1
-      vim.g.gruvbox_material_enable_italic = 0
-      vim.g.gruvbox_material_enable_bold = 0
-      vim.g.gruvbox_material_transparent_background = 1
-      vim.g.gruvbox_material_foreground = "mix"
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_ui_contrast = "high"
-      vim.g.gruvbox_material_float_style = "dim"
-    end,
   },
 }
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,9 +1,11 @@
 return {
   -- disable trouble
+  { "indent-blankline.nvim", enabled = false },
   { "kinsho/bufferline.nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
 }
