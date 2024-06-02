a4b825dac00b92e80298ed3c7571473313696a9a
gabriel-suela <gscsuela@gmail.com>
2024-06-01 22:09:53 -0300

    refactor: move from lazynvim(distro) to custom cfg

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,62 +1,36 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-vim.scriptencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
+vim.opt.guicursor = ""
 
-vim.opt.number = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.opt.backup = false
-vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.shell = "zsh"
-vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
-vim.opt.inccommand = "split"
-vim.opt.ignorecase = true
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.wrap = true
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.path:append({ "**" })
-vim.opt.wildignore:append({ "*/node_modules/*" })
--- vim.opt.colorcolumn = "80"
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.splitkeep = "cursor"
-vim.opt.mouse = "a"
-vim.opt.formatoptions:append({ "r" })
-vim.opt.laststatus = 0
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
-vim.opt.clipboard = "unnamedplus"
+vim.opt.smartindent = true
+vim.opt.smartcase = true
 
-if vim.fn.has("wsl") == 1 then
-  vim.api.nvim_create_autocmd("TextYankPost", {
+vim.opt.wrap = false
 
-    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
-    callback = function()
-      vim.fn.system("clip.exe", vim.fn.getreg('"'))
-    end,
-  })
-end
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
--- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
+vim.opt.termguicolors = true
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = true,
-})
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
-vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
+vim.opt.updatetime = 50
+vim.opt.conceallevel = 2
+
 vim.cmd([[au BufNewFile,BufRead *.yaml.gotmpl setf yaml]])
-vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
