f2e686ab142ceb839daec90cd5e0ed64a73b9803
gabriel-suela <gscsuela@gmail.com>
2024-07-08 14:16:08 -0300

    update prettier conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,31 +1,40 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
-
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        json = { { "prettierd", "prettier" } },
-        bash = { "beautysh" },
-        yaml = { "prettier, prettierd" },
-        sh = { { "shellcheck" } },
-        python = { "isort", "black" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
-    vim.keymap.set({ "n", "v" }, "<leader>fd", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 10000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { { "prettierd", "prettier" } },
+				json = { "prettier" },
+				bash = { "beautysh" },
+				yaml = { "prettier" },
+				sh = { { "shellcheck" } },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+			formatters = {
+				prettier = {
+					options = {
+						ft_parsers = {
+							yaml = "yaml",
+						},
+						ignore_errors = true,
+					},
+				},
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "<leader>fd", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 10000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
