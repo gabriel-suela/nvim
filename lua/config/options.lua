837b75598302142c3ca89816e6a0e22eefba7996
gabriel-suela <gscsuela@gmail.com>
2024-10-24 11:10:38 -0300

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -42,18 +42,18 @@ vim.cmd([[au BufNewFile,BufRead *.yaml.gotmpl setf yaml]])
 vim.opt.fileformats = { "unix", "dos" }
 
 -- wsl only
-vim.g.clipboard = {
-  name = 'win32-clip',
-  copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
-  },
-  paste = {
-    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-  cache_enabled = 1,
-}
+-- vim.g.clipboard = {
+--   name = 'win32-clip',
+--   copy = {
+--     ['+'] = 'clip.exe',
+--     ['*'] = 'clip.exe',
+--   },
+--   paste = {
+--     ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--   },
+--   cache_enabled = 1,
+-- }
 
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 --   virtual_text = true,
