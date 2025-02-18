d270b0270e83a3363e582e2950fef36857e62b57
gabriel-suela <gscsuela@gmail.com>
2025-02-18 15:07:50 -0300

    remove unused colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,14 +11,6 @@ return {
     },
   },
   {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = false
-    },
-  },
-  {
     "Mofiqul/vscode.nvim",
     priority = 100,
     config = function() end,
@@ -27,89 +19,5 @@ return {
     "dgox16/oldworld.nvim",
     lazy = false,
     priority = 1000,
-  },
-  {
-    "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("everforest").setup({
-        -- Your config here
-      })
-    end,
-  },
-  {
-
-    "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
-    -- you can set set configuration options here
-    -- config = function()
-    --     vim.g.zenbones_darken_comments = 45
-    --     vim.cmd.colorscheme('zenbones')
-    -- end
-
-  },
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    priority = 1000,
-    name = "catppuccin",
-    opts = {
-      transparent_background = false,
-    },
-  },
-  {
-    "sainnhe/gruvbox-material",
-    name = "gruvbox-material",
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_better_performance = 1
-      -- Fonts
-      vim.g.gruvbox_material_disable_italic_comment = 1
-      vim.g.gruvbox_material_enable_italic = 0
-      vim.g.gruvbox_material_enable_bold = 0
-      vim.g.gruvbox_material_transparent_background = 0
-      -- Themes
-      vim.g.gruvbox_material_foreground = "mix"
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
-      vim.g.gruvbox_material_float_style = "dim"  -- Background of floating windows
-      local configuration = vim.fn["gruvbox_material#get_configuration"]()
-      local palette = vim.fn["gruvbox_material#get_palette"](
-        configuration.background,
-        configuration.foreground,
-        configuration.colors_override
-      )
-      local highlights_groups = {
-        FoldColumn = { bg = "none" },
-        SignColumn = { bg = "none" },
-        Normal = { bg = "none" },
-        NormalNC = { bg = "none" },
-        NormalFloat = { bg = "none" },
-        FloatBorder = { bg = "none" },
-        TelescopeBorder = { bg = "none" },
-        TelescopeNormal = { fg = "none" },
-        TelescopePromptNormal = { bg = "none" },
-        TelescopeResultsNormal = { bg = "none" },
-        Visual = { bg = palette.bg_statusline3[1] },
-        TelescopeSelection = { bg = palette.bg_visual_blue[1] },
-        ColorColumn = { bg = palette.bg_visual_blue[1] },
-        CursorLine = { bg = palette.bg0[1], blend = 15 },
-        GitSignsAdd = { fg = palette.green[1], bg = "none" },
-        GitSignsChange = { fg = palette.yellow[1], bg = "none" },
-        GitSignsDelete = { fg = palette.red[1], bg = "none" },
-        IndentBlanklineContextChar = { fg = palette.grey1[1] },
-        IndentBlanklineIndent1 = { fg = palette.bg5[1] },
-      }
-      for group, styles in pairs(highlights_groups) do
-        vim.api.nvim_set_hl(0, group, styles)
-      end
-    end,
   },
 }
