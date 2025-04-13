54bb09e011b11fce5131ff6ac89553ebedf9404b
gabriel-suela <gscsuela@gmail.com>
2025-04-12 22:48:32 -0300

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -42,6 +42,22 @@ vim.cmd([[au BufNewFile,BufRead *.yaml.gotmpl setf yaml]])
 
 vim.opt.fileformats = { "unix", "dos" }
 
+
+
+
+if vim.fn.has('wsl') == 1 then
+  vim.api.nvim_create_autocmd('TextYankPost', {
+
+    group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+
+    callback = function()
+      vim.fn.system('clip.exe', vim.fn.getreg('"'))
+    end,
+
+  })
+end
+
+
 vim.diagnostic.config({
   virtual_text = true, -- Exibe mensagens inline
   signs = {
